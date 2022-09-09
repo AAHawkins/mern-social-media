e03d299bb59929fb1e49442fbd82da32fafc6f18
Alicia Hawkins <a_smokes@hotmail.com>
2022-09-08 22:26:49 -0500

    Complete redux framework

--- client/src/components/form/styles.js
+++ client/src/components/form/styles.js
@@ -12,7 +12,8 @@ export default makeStyles((theme) => ({
   form: {
     display: 'flex',
     flexWrap: 'wrap',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    padding: '1.5rem'
   },
   fileInput: {
     width: '97%',
